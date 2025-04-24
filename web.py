1e33d6cc31e6d5b1399c01ccce4527f2117e84a7
karthi-s-13 <karthikeyan13ind@gmail.com>
2025-04-24 10:01:09 +0530

    Succes

--- web.py
+++ web.py
@@ -1,44 +1,6 @@
-# EX01 Developing a Simple Webserver
-## Date: 24.04.2025
-
-## AIM:
-To develop a simple webserver to serve html pages and display the list of protocols in TCP/IP Protocol Suite.
-
-## DESIGN STEPS:
-### Step 1: 
-HTML content creation.
-
-### Step 2:
-Design of webserver workflow.
-
-### Step 3:
-Implementation using Python code.
-
-### Step 4:
-Import the necessary modules.
-
-### Step 5:
-Define a custom request handler.
-
-### Step 6:
-Start an HTTP server on a specific port.
-
-### Step 7:
-Run the Python script to serve web pages.
-
-### Step 8:
-Serve the HTML pages.
-
-### Step 9:
-Start the server script and check for errors.
-
-### Step 10:
-Open a browser and navigate to http://127.0.0.1:8000 (or the assigned port).
-
-## PROGRAM:
-```
 from django.shortcuts import render,HttpResponse
 from http.server import HTTPServer,BaseHTTPRequestHandler
+
 content='''
 <!DOCTYPE html>
 <html lang="en">
@@ -111,9 +73,6 @@ content='''
     </body>
 </html>
 '''
-```
-
-```
 class Myserver(BaseHTTPRequestHandler):
     def do_GET(self):
         print("Get request received...")
@@ -125,14 +84,3 @@ print("This is my webserver")
 serveraddress=('',8000)
 httpd = HTTPServer(serveraddress,Myserver)
 httpd.serve_forever()
-
-```
-
-## OUTPUT:
-![alt text](<Screenshot 2025-04-24 095439.png>)
-
-![alt text](image.png)
-
-
-## RESULT:
-The program for implementing simple webserver is executed successfully.
